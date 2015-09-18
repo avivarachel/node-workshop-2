360f5d6b55ab773f3573616202424ef0db5366b3
Aviva Wolpert <wolpertaviva@gmail.com>
2015-09-18 16:30:02 +0000

    web server

---
+++ server.js
@@ -0,0 +1,9 @@
+// A tiny server
+var http = require('http');
+var requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Hello, World!\n');
+}
+
+var server = http.createServer(requestListener);
+server.listen(process.env.PORT, process.env.IP);
